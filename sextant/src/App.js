79d5d71c48a5fa0063459bf28ba43ea50e1f9ebd
Arcadia Parson <aparson@catalyte.io>
2022-08-25 12:59:32 +1000

    added notecard component file

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,9 +1,14 @@
+import { React, ReactDom } from 'react';
+import InfoCard from './components/info-card';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      
+      <section>
+        <InfoCard />
+      </section>
     </div>
   );
 }
